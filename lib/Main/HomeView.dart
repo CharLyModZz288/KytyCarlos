33555f0d38bffe8ad12e43afcb8c96f40e73638c
CharLyModZz288 <126071634+CharLyModZz288@users.noreply.github.com>
2023-10-13 20:34:13 +0200

    cambiamos de nuevo el home para hacer el gridview

--- lib/Main/HomeView.dart
+++ lib/Main/HomeView.dart
@@ -47,22 +47,22 @@ class _HomeViewState extends State<HomeView> {
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(title: Text("KYTY"),),
-      body: ListView.separated(
+      /*body: ListView.separated(
         padding: EdgeInsets.all(8),
         itemCount: posts.length,
         itemBuilder: creadorDeItemLista,
         separatorBuilder: creadorDeSeparadorLista,
-      ),
-      /*body: Center(
+      ),*/
+      body: Center(
         child:
         GridView.builder(
             gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
+                crossAxisCount: 1,
             ),
             itemCount: posts.length,
             itemBuilder: creadorDeItemLista,
             ),
-      ),*/
+      ),
     );
   }
 
