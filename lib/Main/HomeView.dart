8ab7dbb02b8d7036994c95c5dfa8eb6a1dea3005
CharLyModZz288 <126071634+CharLyModZz288@users.noreply.github.com>
2023-10-13 20:39:22 +0200

    Como tuve un problema al pushsear en clase he tenido que usar un proyecto que ya tenia en casa como te comente anteriormente esta empezado desde el commit 23

--- lib/Main/HomeView.dart
+++ lib/Main/HomeView.dart
@@ -57,7 +57,7 @@ class _HomeViewState extends State<HomeView> {
         child:
         GridView.builder(
             gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
+                crossAxisCount: 2,
             ),
             itemCount: posts.length,
             itemBuilder: creadorDeItemLista,
