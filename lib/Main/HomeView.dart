fe3cfe5c508330c635f735af8152c5a1e5b42151
alumno1 <caesse280801@gmail.com>
2023-11-10 12:50:48 +0100

    Con la vista DrawerClass.dart actualizamos el HomeView.dart

--- lib/Main/HomeView.dart
+++ lib/Main/HomeView.dart
@@ -1,22 +1,68 @@
 
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:kyty/Custom/KTTextField.dart';
 import 'package:kyty/FirestoreObjects/FbPost.dart';
+import 'package:kyty/OnBoarding/LoginView.dart';
+import 'package:kyty/Singletone/DataHolder.dart';
 
+import '../Custom/BottomMenu.dart';
 import '../Custom/PostCellView.dart';
+import '../Custom/PostGridCellView.dart';
+import '../Custom/DrawerClass.dart';
 
-class HomeView extends StatefulWidget{
+
+class HomeView extends StatefulWidget {
 
 
   @override
   State<HomeView> createState() => _HomeViewState();
 }
 
-class _HomeViewState extends State<HomeView> {
+class _HomeViewState extends State<HomeView>{
 
   FirebaseFirestore db = FirebaseFirestore.instance;
   final List<FbPost> posts = [];
+  bool bIsList=false;
+  String eve="Hola";
+
+
+  void onBottonMenuPressed(int indice) {
+    // TODO: implement onBottonMenuPressed
+    print("------>>>> HOME!!!!!!"+indice.toString()+"---->>> ");
+    setState(() {
+      if(indice == 0){
+        bIsList=true;
+      }
+      else if(indice==1){
+        bIsList=false;
+      }
+      else if(indice==2){
+        exit(0);
+      }
+    });
+
+  }
+
+  void fHomeViewDrawerOnTap(int indice){
+    print("---->>>> "+indice.toString());
+    if(indice==0){
+      FirebaseAuth.instance.signOut();
+      //Navigator.of(context).pop();
+      //Navigator.of(context).popAndPushNamed("/loginview");
+      //Navigator.of(context).pushAndRemoveUntil(newRoute, (route) => false)
+      Navigator.of(context).pushAndRemoveUntil (
+        MaterialPageRoute (builder: (BuildContext context) =>  LoginView()),
+        ModalRoute.withName('/loginview'),
+      );
+    }
+    else if(indice==1){
+      exit(0);
+    }
+  }
 
   @override
   void initState() {
@@ -40,6 +86,7 @@ class _HomeViewState extends State<HomeView> {
 
     }
 
+
   }
 
   @override
@@ -47,29 +94,49 @@ class _HomeViewState extends State<HomeView> {
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(title: Text("KYTY"),),
-      /*body: ListView.separated(
-        padding: EdgeInsets.all(8),
-        itemCount: posts.length,
-        itemBuilder: creadorDeItemLista,
-        separatorBuilder: creadorDeSeparadorLista,
-      ),*/
       body: Center(
-        child:
-        GridView.builder(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-            ),
-            itemCount: posts.length,
-            itemBuilder: creadorDeItemLista,
-            ),
+        child: celdasOLista(bIsList),
       ),
+      bottomNavigationBar: BottomMenu(onBotonesClicked: this.onBottonMenuPressed),
+      drawer: DrawerClass(onItemTap: fHomeViewDrawerOnTap,),
+      floatingActionButton:FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).pushNamed("/postcreateview");
+        },
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.miniStartFloat,
+      /**/
     );
   }
 
+  void onItemListClicked(int index){
+    DataHolder().selectedPost=posts[index];
+    DataHolder().saveSelectedPostInCache();
+    Navigator.of(context).pushNamed("/postview");
+    //print("EL ELEMENTO DE LA LISTA QUE ACABA DE TOCARSE ES> "+index.toString());
+
+  }
+
+  /**
+   *
+   */
   Widget? creadorDeItemLista(BuildContext context, int index){
     return PostCellView(sText: posts[index].titulo,
-      dFontSize: 20,
-      iColorCode: 0,
+        dFontSize: 30,
+        iColorCode: 0,
+        iPosicion: index,
+        onItemListClickedFun:onItemListClicked
+    );
+  }
+
+  Widget? creadorDeItemMatriz(BuildContext context, int index){
+    return PostGridCellView(sText: posts[index].titulo,
+        dFontSize: 28,
+        iColorCode: 0,
+        dHeight: 300,
+        iPosicion: index,
+        onItemListClickedFun:onItemListClicked
     );
   }
 
@@ -83,5 +150,24 @@ class _HomeViewState extends State<HomeView> {
       ],
     );
   }
+
+  Widget celdasOLista(bool isList) {
+    if (isList) {
+      return ListView.separated(
+        padding: EdgeInsets.all(8),
+        itemCount: posts.length,
+        itemBuilder: creadorDeItemLista,
+        separatorBuilder: creadorDeSeparadorLista,
+      );
+    } else {
+      return GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 5),
+          itemCount: posts.length,
+          itemBuilder: creadorDeItemMatriz
+      );
+    }
+  }
+
+
 
 }
