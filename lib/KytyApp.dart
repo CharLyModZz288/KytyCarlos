562b7e967e07bd51a10fb1e493c543fe2dca33e2
alumno1 <caesse280801@gmail.com>
2023-11-10 13:13:36 +0100

    Mejoramos el KytyApp.dart para meter la opcion de sms

--- lib/KytyApp.dart
+++ lib/KytyApp.dart
@@ -1,9 +1,13 @@
 
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:kyty/Main/HomeView.dart';
+import 'package:kyty/OnBoarding/PhoneLoginView.dart';
 import 'package:kyty/Splash/SplashView.dart';
 
+import 'Main/PostCreateView.dart';
+import 'Main/PostView.dart';
 import 'OnBoarding/LoginView.dart';
 import 'OnBoarding/PerfilView.dart';
 import 'OnBoarding/RegisterView.dart';
@@ -13,19 +17,38 @@ class KytyApp extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-   MaterialApp materialApp=MaterialApp(title: "KyTy Miau!",
-       routes: {
-         '/loginview':(context) => LoginView(),
-         '/registerview':(context) => RegisterView(),
-         '/homeview':(context) => HomeView(),
-         '/splashview':(context) => SplashView(),
-         '/perfilview':(context) => PerfilView(),
+    MaterialApp materialApp;
+    if (kIsWeb) {
+      materialApp=MaterialApp(title: "KyTy Miau!",
+        routes: {
+          '/loginview':(context) => LoginView(),
+          '/registerview':(context) => RegisterView(),
+          '/homeview':(context) => HomeView(),
+          '/splashview':(context) => SplashView(),
+          '/perfilview':(context) => PerfilView(),
+          '/postview':(context) => PostView(),
+          '/postcreateview':(context) => PostCreateView(),
+        },
+        initialRoute: '/homeview',
+      );
+    }
+    else{
+      materialApp=MaterialApp(title: "KyTy Miau!",
+        routes: {
+          '/loginview':(context) => PhoneLoginView(),
+          '/registerview':(context) => RegisterView(),
+          '/homeview':(context) => HomeView(),
+          '/splashview':(context) => SplashView(),
+          '/perfilview':(context) => PerfilView(),
+          '/postview':(context) => PostView(),
+          '/postcreateview':(context) => PostCreateView(),
+        },
+        initialRoute: '/splashview',
+      );
+    }
 
-       },
-     initialRoute: '/homeview',
-   );
 
-   return materialApp;
+    return materialApp;
   }
 
 }
